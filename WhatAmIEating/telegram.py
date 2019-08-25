87e2546aa1ed2761292fe3a39b8ae63c6d32dc20
nekojess1 <jessi_alves1@outlook.com>
2019-08-25 05:45:23 -0300

    alterando o token

--- WhatAmIEating/telegram.py
+++ WhatAmIEating/telegram.py
@@ -1,7 +1,7 @@
 import telepot
 from Chatbot import Chatbot
 
-telegram = telepot.Bot("905297180:AAFIGPAg0PJm60b7M-NsX2Ka6fNwlvjjeWA")
+telegram = telepot.Bot("786457294:AAEtBN-c5QtkUzj150Lar2V6QfaPDMChRHY")
 bot = Chatbot("Nexus")
 def recebendomensagem(msg):
     frase = bot.escuta(frase=msg['text'])
